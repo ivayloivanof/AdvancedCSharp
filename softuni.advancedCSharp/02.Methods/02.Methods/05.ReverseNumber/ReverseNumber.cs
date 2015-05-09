13e0708bbe1c2d0ae4151223a29816aad78de7bf
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-09 22:31:11 +0300

    Add problem 05.Reverse Double Number and solution of the problem

---
+++ softuni.advancedCSharp/02.Methods/02.Methods/05.ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class ReverseNumber
+{
+    static void Main()
+    {
+        double number = double.Parse(Console.ReadLine());
+        Console.WriteLine(GetReversedNumber(number));
+    }
+
+    public static double GetReversedNumber(double number)
+    {
+        string reverse = number.ToString();
+        char[] charArray = reverse.ToCharArray();
+        Array.Reverse(charArray);
+        reverse = new string(charArray);
+        return Convert.ToDouble(reverse);
+    }
+}
