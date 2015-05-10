5ba1b2a66395a76b7d70d21e5170cc7e71656407
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-10 17:25:13 +0300

    Add problem o4.SequenceInMatrix, but don't decision

---
+++ AdvancedCSharp/02.MuldimentionalArraysSetsDictionaries/MultiArraySetsDict/04.SequenceInMatrix/SequenceInMatrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class SequenceInMatrix
+{
+    static void Main()
+    {
+        int[] field = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+        string[,] matrix = new string[field[0] + 1, field[1] + 1];
+        CreateMatrix(field, matrix);
+        Console.WriteLine();
+        ConsoleWriteMatrix(matrix);
+    }
+
+    private static string[,] CreateMatrix(int[] field, string[,] matrix)
+    {
+        for (int i = 0; i <= field[0]; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            for (int j = 0; j <= field[1]; j++)
+            {
+                matrix[i, j] = numbers[j].Trim();
+            }
+        }
+        return matrix;
+    }
+
+    private static void ConsoleWriteMatrix(string[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write("{0,5}", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
