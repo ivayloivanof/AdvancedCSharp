9c13a79a6483c14b79da559585332c1b05a647b5
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-12 00:32:50 +0300

    Add Project folder and ReverseString and StringLenght problems

---
+++ AdvancedCSharp/03.StringsAndTextProcessing/StringAndTextProcessing/01.ReverseString/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class ReverseString
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        for (int i = text.Length; i > 0; i--)
+        {
+            Console.Write(text[i-1]);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ AdvancedCSharp/03.StringsAndTextProcessing/StringAndTextProcessing/02.StringLength/StringLenght.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class StringLenght
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        if (text.Length < 20)
+            Console.WriteLine(text+ new string('*', 20 - text.Length));
+        else
+            Console.WriteLine(text.Substring(0, 20));
+    }
+}
