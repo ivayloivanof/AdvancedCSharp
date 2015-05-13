c0dd3b907342bafb896e80545a6ac1d9204c8c36
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-13 19:26:01 +0300

    Add InsertionSort project and add decision

---
+++ AdvancedCSharpAlgorithmsLab/03.InsertionSort/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class InsertionSort
+{
+    static void Main()
+    {
+        int[] integer = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+        Console.WriteLine(String.Join(" ", PerformInsertionSort(integer)));
+    }
+
+    static int[] PerformInsertionSort(int[] inputArray)
+    {
+        for (int i = 0; i < inputArray.Length - 1; i++)
+        {
+            for (int j = i + 1; j > 0; j--)
+            {
+                if (inputArray[j - 1] > inputArray[j])
+                {
+                    int temp = inputArray[j - 1];
+                    inputArray[j - 1] = inputArray[j];
+                    inputArray[j] = temp;
+                }
+            }
+        }
+        return inputArray;
+    }
+}
