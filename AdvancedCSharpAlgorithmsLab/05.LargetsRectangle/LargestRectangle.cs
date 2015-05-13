1fd79494ce17a7c74fe6eb1597ae066043a02811
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-13 20:47:46 +0300

    Add LargestRectangle project

---
+++ AdvancedCSharpAlgorithmsLab/05.LargetsRectangle/LargestRectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LargestRectangle
+{
+    static void Main()
+    {
+        string row = Console.ReadLine();
+        List<string> text = new List<string>();
+
+        while (!row.Contains("END"))
+        {
+            row = Console.ReadLine();
+            string rows = row.Substring(1, row.Length-2);
+            string[] character = rows.Split(',');
+            text.Add(rows);
+        }
+        Console.WriteLine(string.Join("", text));
+    }
+}
