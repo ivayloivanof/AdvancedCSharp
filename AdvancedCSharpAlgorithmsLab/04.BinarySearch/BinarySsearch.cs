2ef2b74cb2fafb0fd2f64699fef4711844b3e820
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-13 20:28:09 +0300

    Add BinarySearch project

---
+++ AdvancedCSharpAlgorithmsLab/04.BinarySearch/BinarySsearch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+class BinarySsearch
+{
+    static void Main()
+    {
+        int[] integer = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+        int searchNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine(BinarySearch(integer, searchNumber));
+        //Console.WriteLine(Array.BinarySearch(integer, 0, integer.Length));
+    }
+
+    private static int BinarySearch(int[] numbers, int searchNumber)
+    {
+        Array.Sort(numbers);
+        //int low = 0, high = numbers.Length - 1, position = 0;
+        
+        //while (low <= high)
+        //{
+        //    position = low + ((high - low) / 2);
+
+        //    if (searchNumber == numbers[position])
+        //        return position;
+        //    else if (searchNumber < numbers[position])
+        //        high = position - 1;
+        //    else
+        //        low = position + 1;
+        //}
+
+        int position = numbers.Length/2;
+        bool loop = true;
+        int notfound = -1;
+        while (loop)
+        {
+            if (numbers[position] == searchNumber)
+                return position;
+            else if (numbers[position] < searchNumber)
+                position++;
+            else if (numbers[position] > searchNumber)
+                position--;
+            else
+                loop = false;
+
+            if (position < 0 || position > numbers.Length - 1)
+                return notfound;
+        }
+
+        return notfound;
+    }
+}
