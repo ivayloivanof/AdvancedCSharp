0cf630556e15ed7bf773bdfcbb286601840c1811
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-15 22:53:00 +0300

    Decided SerriesOfLetters project

--- AdvancedCSharp/04.RegularEpression/RegularExpression/01.SerriesOfLetters/SerriesOfLetters.cs
+++ AdvancedCSharp/04.RegularEpression/RegularExpression/01.SerriesOfLetters/SerriesOfLetters.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 class SerriesOfLetters
 {
     static void Main()
     {
-        string text = Console.ReadLine();
-        string pattern = @"";
+        string textForCleaning = Console.ReadLine();
+        string pattern = @"([a-z])\1*";
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = Regex.Matches(textForCleaning, pattern);
+
+        for (int i = 0; i < matches.Count; i++)
+        {
+            string tempPattern = matches[i].ToString();
+            string replace = tempPattern.First().ToString();
+            Regex tempRegex = new Regex(tempPattern);
+            textForCleaning = tempRegex.Replace(textForCleaning, replace);
+        }
+
+        Console.WriteLine(textForCleaning);
     }
 }
