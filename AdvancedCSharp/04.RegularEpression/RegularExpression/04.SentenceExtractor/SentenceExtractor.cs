4d5c244899fbcba35058afe1f89d035ca5b07f1e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-16 22:40:38 +0300

    SentenceExtractor decided

---
+++ AdvancedCSharp/04.RegularEpression/RegularExpression/04.SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class SentenceExtractor
+{
+    static void Main()
+    {
+        string pattern = Console.ReadLine();
+        string text = Console.ReadLine();
+        string[] lines = Regex.Split(text, @"(?<=[.!?])\s+");
+        foreach (string line in lines)
+        {
+            MatchCollection match = Regex.Matches(line, @"\s"+pattern+ @"\s+([\w]+)");
+            if (match.Count != 0)
+                Console.WriteLine(line.Trim());
+        }
+        Console.WriteLine();
+    }
+}
