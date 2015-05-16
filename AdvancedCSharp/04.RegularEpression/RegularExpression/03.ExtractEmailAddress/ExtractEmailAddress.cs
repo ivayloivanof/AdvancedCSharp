6c874ada19553b848ca966baf2c49770a562746c
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-16 22:06:46 +0300

    Decided Email extraxt problem and fixed #10

--- AdvancedCSharp/04.RegularEpression/RegularExpression/03.ExtractEmailAddress/ExtractEmailAddress.cs
+++ AdvancedCSharp/04.RegularEpression/RegularExpression/03.ExtractEmailAddress/ExtractEmailAddress.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 class ExtractEmailAddress
 {
     static void Main()
     {
+        string text = Console.ReadLine();
+
+        string pattern = @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+        Regex regex = new Regex(pattern);
+        MatchCollection email = regex.Matches(text);
+        foreach (Match match in email)
+        {
+            Console.WriteLine(match);
+        }
 
     }
 }
