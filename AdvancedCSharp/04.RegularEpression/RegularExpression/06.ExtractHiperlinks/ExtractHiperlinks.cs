3484a0311c2c7dd4cf8794294de0c0cecb6ab5e4
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-17 22:31:00 +0300

    Decided ExtractHiperlinks project

---
+++ AdvancedCSharp/04.RegularEpression/RegularExpression/06.ExtractHiperlinks/ExtractHiperlinks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExtractHyperlinks
+{
+    static void Main(string[] args)
+    {
+        string inputLine;
+        StringBuilder sb = new StringBuilder();
+        while (!((inputLine = Console.ReadLine()) == "END"))
+        {
+            sb.Append(inputLine);
+        }
+        string text = sb.ToString();
+        string pattern = @"<a\s+(?:[^>]+\s+)?href\s*=\s*(?:'(?<url>[^']*)'|""(?<url>[^""]*)""|(?<url>[^\s>]+))[^>]*>";
+        Regex links = new Regex(pattern);
+        MatchCollection matches = links.Matches(text);
+        foreach (Match hyperlink in matches)
+        {
+            Console.WriteLine(hyperlink.Groups["url"]);
+        }
+    }
+}
