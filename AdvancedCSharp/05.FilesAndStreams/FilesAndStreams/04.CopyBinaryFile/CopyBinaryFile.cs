7be646e32e416150e3e6127a0b06cca97da4b81e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-21 21:18:10 +0300

    Add CopyBinaryFile and decided

---
+++ AdvancedCSharp/05.FilesAndStreams/FilesAndStreams/04.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class CopyBinaryFile
+{
+    private static string imagePath = @"../../fire.jpg";
+    private static string newImagePath = @"../../fire-new.jpg";
+
+    static void Main()
+    {
+        using (var source = new FileStream(imagePath, FileMode.Open))
+        {
+            using (var destination = new FileStream(newImagePath, FileMode.Create))
+            {
+                double fileLength = source.Length;
+                byte[] buffer = new byte[4096];
+                while (true)
+                {
+                    int readBytes = source.Read(buffer, 0, buffer.Length);
+                    if (readBytes == 0)
+                    {
+                        break;
+                    }
+
+                    destination.Write(buffer, 0, readBytes);
+
+                    Console.WriteLine("{0:P}", Math.Min(source.Position / fileLength, 1));
+                }
+            }
+        }
+    }
+}
