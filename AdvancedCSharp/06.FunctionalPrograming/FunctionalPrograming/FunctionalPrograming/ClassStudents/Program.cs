ad3b7f6c5f2188cc30c7a22e403f300db45baf7a
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-05-22 21:55:31 +0300

    update TWPS

---
+++ AdvancedCSharp/06.FunctionalPrograming/FunctionalPrograming/FunctionalPrograming/ClassStudents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassStudents
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+
+}
---
+++ AdvancedCSharp/06.FunctionalPrograming/FunctionalPrograming/FunctionalPrograming/ClassStudents/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassStudents
+{
+	public class Students
+	{
+	    public string FirstName { get; set; }
+	    public string LastName { get; set; }
+	    public int Age { get; set; }
+	    public string FacultyNumber { get; set; }
+	    public string Phone { get; set; }
+	    public string Email { get; set; }
+	    public string GroupNumber { get; set; }
+
+	}
+}
+
