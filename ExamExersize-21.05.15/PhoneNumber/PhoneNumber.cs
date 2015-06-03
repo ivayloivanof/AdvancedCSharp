16fccda27bc3cb1924e6a6099ea9b179a58da5d1
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-03 22:05:05 +0300

    update

---
+++ ExamExersize-21.05.15/PhoneNumber/PhoneNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PhoneNumber
+{
+    class PhoneNumber
+    {
+        static void Main()
+        {
+            string pattern = @"([a-zA-Z]+)|([\+\.\/\-\s\(\)]?[0-9]+[\+\.\/\-\(\)]?)";
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line.Contains("END"))
+                    break;
+
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = regex.Matches(line);
+
+
+                Console.WriteLine();
+            }
+            //<b>[name]:</b> [phone number] 
+        }
+    }
+}
